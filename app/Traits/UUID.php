769edca3fb96a531d9d787f72ec87ce6af9dcf0b
Burak Sevinc <burak@Buraks-MacBook-Pro.local>
2023-08-04 13:05:35 +0300

    feat: UUID trait created

---
+++ app/Traits/UUID.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait UUID
+{
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->getKey())) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
