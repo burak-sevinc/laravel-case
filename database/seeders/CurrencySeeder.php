58883780c3672219e7a25cd9dfafd4e0c04fb41d
Burak Sevinc <burak@Buraks-MacBook-Pro.local>
2023-08-06 17:47:42 +0300

    feat: timezone changed

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,14 +14,15 @@ class CurrencySeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('currencies')->insert([
             'id' => '11540e34-4cd1-448e-af98-a1b14a5abd44',
             'currency_code' => 'USD',
             'long_name' => 'Amerikan Doları',
             'symbol' => '$',
             'created_by' => 1,
-            'created_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
-            'updated_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
 
         DB::table('currencies')->insert([
@@ -29,8 +31,8 @@ class CurrencySeeder extends Seeder
             'long_name' => 'Euro',
             'symbol' => '€',
             'created_by' => 1,
-            'created_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
-            'updated_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
     }
 }
--- database/seeders/CurrencyValuesSeeder.php
+++ database/seeders/CurrencyValuesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,50 +14,41 @@ class CurrencyValuesSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('currency_values')->insert([
             'id' => 'c182ea3c-4de5-4213-bd59-332dbfabea18',
             'currency_id' => '11540e34-4cd1-448e-af98-a1b14a5abd44',
             'currency_value' => 26.9502,
-            'logged_at' => (new \DateTimeImmutable('now', new \DateTimeZone('Europe/Istanbul')))
-                ->sub(new \DateInterval('P1D'))
-                ->format(DATE_ATOM),
-            'created_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
-            'updated_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
+            'logged_at' => $now,
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
-
 
         DB::table('currency_values')->insert([
             'id' => '695a081d-a8e1-418d-a02c-d9590cb8a2ea',
             'currency_id' => 'c961cb13-4d6f-44b1-9c41-c1115359daaa',
             'currency_value' => 29.6118,
-            'logged_at' => (new \DateTimeImmutable('now', new \DateTimeZone('Europe/Istanbul')))
-                ->sub(new \DateInterval('P1D'))
-                ->format(DATE_ATOM),
-            'created_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
-            'updated_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
+            'logged_at' => $now,
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
 
         DB::table('currency_values')->insert([
             'id' => '74c737e8-fdc5-4c6d-9d21-2f660c382954',
             'currency_id' => '11540e34-4cd1-448e-af98-a1b14a5abd44',
             'currency_value' => 26.9325,
-            'logged_at' => (new \DateTimeImmutable(
-                'now',
-                new \DateTimeZone('Europe/Istanbul')
-            ))->format(DATE_ATOM),
-            'created_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
-            'updated_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
+            'logged_at' => $now,
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
-
 
         DB::table('currency_values')->insert([
             'id' => 'c90a9426-775f-4015-bad2-d6c22dbec3a3',
             'currency_id' => 'c961cb13-4d6f-44b1-9c41-c1115359daaa',
             'currency_value' => 29.6762,
-            'logged_at' => (new \DateTimeImmutable('now', new \DateTimeZone('Europe/Istanbul')))
-                ->format(DATE_ATOM),
-            'created_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
-            'updated_at' => (new \DateTimeImmutable())->format(DATE_ATOM),
+            'logged_at' => $now,
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
     }
 }
